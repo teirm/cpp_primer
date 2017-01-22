0355c777219eb0c3f595b7990a014b48b0a47bbe
teirm <tribunus.vii@gmail.com>
2017-01-21 20:32:32 -0500

    Completed exercise_332

---
+++ ch_3/exercise_332.cpp
@@ -0,0 +1,57 @@
+/* Exercise 3.3.2
+ * Purpose: Section Exercises
+ */
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "../tools/display_ex.cpp"
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+using std::string;
+
+using std::vector;
+
+int append_ints()
+{
+    int input = 0;
+
+    vector<int> vec_i;
+
+    while (cin >> input) {
+        vec_i.push_back(input);
+    }
+
+    return 0;
+}
+
+int append_strings()
+{
+    string input;
+
+    vector<string> vec_s;
+
+    while (cin >> input) {
+        vec_s.push_back(input);
+    }
+
+    return 0;
+}
+
+int main()
+{
+
+    display_ex("Exercise 3.14");
+    append_ints();
+
+
+    display_ex("Exercise 3.15");
+    append_strings();
+
+
+    return 0;
+}
